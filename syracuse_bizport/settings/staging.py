11af762e964a137c69127dd55773def883a39a53
Mikela Clemmons <glassresistor@gmail.com>
2016-10-17 18:19:30 -0700

    Added pep8 config file and also fixed all the pep8 errors

--- syracuse_bizport/settings/staging.py
+++ syracuse_bizport/settings/staging.py
@@ -1,19 +1,14 @@
+import dj_database_url
 from __future__ import absolute_import, unicode_literals
-
 from .base import *
+
 
 DEBUG = True
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-	
+DATABASES['default'] = dj_database_url.config()
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
-
-try:
-    from .local import *
-except ImportError:
-    pass
