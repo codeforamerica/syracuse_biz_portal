a7173cddea7bd0cda0177f59a81da1c882332455
Mikela Clemmons <glassresistor@gmail.com>
2016-10-17 18:33:09 -0700

    Made a heroku settings file and made it get the secret key from the environment

--- syracuse_bizport/settings/production.py
+++ syracuse_bizport/settings/production.py
@@ -1,13 +1,4 @@
-import dj_database_url
-from __future__ import absolute_import, unicode_literals
-from .base import *
+import os
+from .heroku import *
 
 DEBUG = False
-
-DATABASES['default'] = dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
--- syracuse_bizport/settings/staging.py
+++ syracuse_bizport/settings/staging.py
@@ -1,14 +1,3 @@
-import dj_database_url
-from __future__ import absolute_import, unicode_literals
-from .base import *
-
+from .heroku import *
 
 DEBUG = True
-
-DATABASES['default'] = dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
