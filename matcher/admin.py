1c7f2935e462e986ced9076b8437d1035d333a3d
Mikela Clemmons <glassresistor@gmail.com>
2016-11-08 15:07:12 -0800

    added application link to columns and made search for title description and application link

--- matcher/admin.py
+++ matcher/admin.py
@@ -10,8 +10,9 @@ from . import forms
 @admin.register(wm_models.get_search_model())
 class OpportunityAdmin(wm_admin.AddCreator):
     form = forms.OpportunityForm
-    list_display = ('title', 'agency', 'updated_at')
+    list_display = ('title', 'agency', 'updated_at', 'application_link')
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
     }
+    search_fields = ['title', 'application_link', 'description']
