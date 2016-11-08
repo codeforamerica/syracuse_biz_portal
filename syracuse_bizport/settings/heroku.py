d4f486a45756051b170c5bbfb2c7d446c5454324
Mikela Clemmons <glassresistor@gmail.com>
2016-11-07 19:41:20 -0800

    added whitenoise settings

--- syracuse_bizport/settings/heroku.py
+++ syracuse_bizport/settings/heroku.py
@@ -13,6 +13,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../staticfiles')
 
-STATIC_ROOT = os.environ['STATIC_ROOT']
-MEDIA_ROOT = os.environ['MEDIA_ROOT']
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
