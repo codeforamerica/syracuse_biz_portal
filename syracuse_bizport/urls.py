1b0c9601412608aece3ae32b2f00a839bd793e9b
Mikela Clemmons <glassresistor@gmail.com>
2016-11-07 19:12:50 -0800

    added urls and whitenoise to wsgi

--- syracuse_bizport/urls.py
+++ syracuse_bizport/urls.py
@@ -1,5 +1,3 @@
-
-
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
@@ -19,13 +17,3 @@ urlpatterns = [
 
     url(r'', include(wagtail_urls)),
 ]
-
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-    # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
--- syracuse_bizport/wsgi.py
+++ syracuse_bizport/wsgi.py
@@ -1,20 +1,4 @@
-"""
-WSGI config for syracuse_bizport project.
+from .wsgi import *
+from whitenoise.django import DjangoWhiteNoise
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
-
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "syracuse_bizport.settings.dev")
-
-application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
