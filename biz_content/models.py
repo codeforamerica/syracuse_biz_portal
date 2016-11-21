2377999b9e5d79b147332b14dbb48bda5b90ff63
Ann <ann.millspaugh@gmail.com>
2016-11-21 12:42:32 -0500

    add checklist items to admin

--- biz_content/models.py
+++ biz_content/models.py
@@ -2,9 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 from wagtail.wagtailsnippets.models import register_snippet
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel, PageChooserPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel, InlinePanel, PageChooserPanel
 from modelcluster.fields import ParentalKey  # Installed with Wagtail, ModelCluster provides many custom field-types that Wagtail relies on
 from modelcluster.models import ClusterableModel
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore import blocks
+
+
 
 # Create your models here.
 
@@ -36,7 +40,7 @@ class Category(ClusterableModel):
         InlinePanel('checklist', label="Checklist")
     ]
 
-class Checklist(ClusterableModel):
+class Checklist(models.Model):
     """
     Represents a checklist for a given Category.
     """
@@ -54,8 +58,19 @@ class Checklist(ClusterableModel):
     )
     category = models.ForeignKey(Category, related_name="checklists", null=True)
 
+    items = StreamField([
+        ('text', blocks.CharBlock(max_length=255,null=True, classname="text", label="Checklist Text")),
+    ], null=True)
+
     def __unicode__(self):
         return self.name
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('description'),
+        FieldPanel('slug'),
+        StreamFieldPanel('items')
+    ]
 
 
 
@@ -63,15 +78,15 @@ class ChecklistItem(models.Model):
     """
     Represents an item in a checklist.
     """
-    checklist = ParentalKey('Checklist', related_name='checklist_items', null=True)
+    # checklist = ParentalKey('Checklist', related_name='checklist_items', null=True)
 
     text = models.CharField(
         max_length=255,null=True
     )
     completed = models.BooleanField(default=False)
     order_num = models.IntegerField(default=0)
-    # checklist = models.ForeignKey(Checklist,
-    #     related_name='checklist_items', null=True)
+    checklist = models.ForeignKey(Checklist,
+        related_name='checklist_items', null=True)
 
     def __unicode__(self):
         return self.text
