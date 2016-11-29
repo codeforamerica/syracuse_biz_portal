211ad2b10ab2fa2dac522cf3bce43e06e6d95d36
Charles Weems <charlie.weems@gmail.com>
2016-11-29 12:09:39 -0800

    Add bucketeer configuration

--- syracuse_bizport/settings/heroku.py
+++ syracuse_bizport/settings/heroku.py
@@ -5,6 +5,11 @@ from .base import *
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
+AWS_ACCESS_ID = os.environ['BUCKETEER_AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['BUCKETEER_AWS_SECRET_ACCESS_KEY']
+
+AWS_STORAGE_BUCKET_NAME = os.environ['BUCKETEER_BUCKET_NAME']
+
 DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
