7eac81df21bb91ecd324c94482e8610046916ac4
Mikela Clemmons <glassresistor@gmail.com>
2016-12-07 19:07:05 -0800

    Got tests running but wagtail stuff can't flush its own tables

--- biz_content/tests/test_forms.py
+++ biz_content/tests/test_forms.py
@@ -1,10 +1,10 @@
 from . import factories
 from biz_content import models, forms
-from django.test import TransactionTestCase
+from django.test import TransactionTestCase, TestCase
 from django.core.urlresolvers import reverse
 
 
-class CheckListFormTestCase(TransactionTestCase):
+class CheckListFormTestCase(TestCase):
     def setUp(self):
         self.project = factories.ProjectFactory()
         self.step_page = factories.StepPageFactory()
@@ -12,7 +12,7 @@ class CheckListFormTestCase(TransactionTestCase):
     def test_init_without_project(self):
         form = forms.ChecklistForm(self.step_page)
         cl = form.fields['checklist']
-        form_qs = cl._queryset.all()
-        qs = self.step_page.checklist_items
-        self.assertQuerysetEqual(form_qs, qs)
+        form_qs = list(cl._queryset.values_list('pk', flat=True))
+        qs = list(self.step_page.checklist_items.values_list('pk', flat=True))
+        self.assertListEqual(form_qs, qs)
 
