9d04daeb22bcded77bbb08dbb82714a6ecbadb18
Ann <ann.millspaugh@gmail.com>
2016-12-07 17:09:01 -0800

    rm databox client until decide pricing

--- syracuse_bizport/settings/heroku.py
+++ syracuse_bizport/settings/heroku.py
@@ -24,5 +24,4 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = "https://%s.s3.amazonaws.com/" % (AWS_ACCESS_KEY_ID)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-DATABOX_KEY = os.environ['DATABOX_KEY']
 
