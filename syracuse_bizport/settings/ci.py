b93ef9ae8e7054e1b73900c69f4c21e2a9553248
Mikela Clemmons <glassresistor@gmail.com>
2016-12-12 13:24:25 -0800

    set postgres user and blank password for travis

--- syracuse_bizport/settings/ci.py
+++ syracuse_bizport/settings/ci.py
@@ -10,6 +10,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'syracuse_biz_portal',
+        'USER': 'postgres',
+        'PASSWORD': '',
     }
 }
 
