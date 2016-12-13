c945c1e847b7cbfb57d70e7d527a059a74473c04
Ann <ann.millspaugh@gmail.com>
2016-12-12 16:24:03 -0800

    pep8 fixes

--- biz_content/forms.py
+++ biz_content/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-
 class ChecklistForm(forms.Form):
 
     def __init__(self, steppage, *args, **kwargs):
@@ -22,14 +21,14 @@ class ChecklistForm(forms.Form):
 
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required = True)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
-    def save(self,commit = True):
-        user = super(CustomUserCreationForm, self).save(commit = False)
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
 
         if commit:
