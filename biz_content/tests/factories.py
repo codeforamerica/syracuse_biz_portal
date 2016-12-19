5f8b96424727a06654b83e5c0f5e31765e163130
Mikela Clemmons <glassresistor@gmail.com>
2016-12-19 13:49:18 -0800

    fixed test by removing date from steppage

--- biz_content/tests/factories.py
+++ biz_content/tests/factories.py
@@ -33,7 +33,6 @@ class StepPageFactory(dfactory.DjangoModelFactory):
     title = factory.sequence(lambda n: "%03d" % n)
     slug = factory.Sequence(lambda n: "%03d" % n)
     description = factory.Sequence(lambda n: "%03d" % n)
-    date = factory.LazyFunction(timezone.now)
     depth = 1
     path = 'test'
     checklist_item1 = factory.RelatedFactory(
