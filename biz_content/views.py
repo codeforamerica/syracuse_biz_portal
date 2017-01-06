a9440eade6b0d2f4e5f32c0238d79c7f5a584e6c
Charles Weems <charlie.weems@gmail.com>
2017-01-05 16:34:54 -0800

    Javascript submit works but un-checking checkbox removes item

--- biz_content/views.py
+++ biz_content/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.http import JsonResponse
 from django.core.exceptions import SuspiciousOperation
@@ -41,6 +41,5 @@ def update_checkbox(request, steppage_id, project_id):
         checked_items = form.save()
     else:
         raise SuspiciousOperation(str(form.POST))
-    return JsonResponse({
-        'checked_items': list(checked_items.values_list('pk', flat=True)),
-    })
+    checked_items = list(checked_items.values_list('pk', flat=True))
+    return redirect(request.META['HTTP_REFERER'])
