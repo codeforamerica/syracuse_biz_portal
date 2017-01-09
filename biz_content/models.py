04378f38382672ebbe86cf05b65d39468e34e810
Charles Weems <charlie.weems@gmail.com>
2017-01-09 11:16:52 -0800

    Add next/previous step function and migration

--- biz_content/models.py
+++ biz_content/models.py
@@ -150,33 +150,24 @@ class StepPage(Page):
         related_name='+'
     )
 
-    next_step = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        on_delete=models.SET_NULL,
-        blank=True,
-        related_name='+',
-        help_text=('Choose the next step to link to at the bottom of the page')
-    )
-
-    previous_step = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        on_delete=models.SET_NULL,
-        blank=True,
-        related_name='+',
-        help_text=('Choose the previous step to link to ',
-                   'at the bottom of the page.')
-    )
-
     content_panels = Page.content_panels + [
         FieldPanel('description'),
         ImageChooserPanel('header_img'),
         StreamFieldPanel('page_content'),
-        FieldPanel('next_step'),
-        FieldPanel('previous_step'),
         InlinePanel('checklist_items', label="Checklist Items"),
     ]
+
+    def previous_step(self):
+        if self.get_prev_sibling():
+            return sefl.get_prev_sibling().url
+        else:
+            return self.get_siblings().last().url
+
+    def next_step(self):
+        if self.get_next_sibling():
+            return self.get_next_sibling().url
+        else:
+            return self.get_siblings().first().url
 
     def get_context(self, request):
         context = super().get_context(request)
