08c33a7c8ee5001eb0aff077bedd4643fb9bfd14
Charles Weems <charlie.weems@gmail.com>
2017-01-24 18:05:00 -0800

    Tests now passing

--- biz_content/tests/test_models.py
+++ biz_content/tests/test_models.py
@@ -9,34 +9,13 @@ class StepPageTestCase(TestCase):
 
     def setUp(self):
         self.user = factories.UserFactory()
-        self.project = self.user.projects.all()[0]
         self.step_page = factories.StepPageFactory()
         self.rf = RequestFactory()
 
     def test_get_context_without_project(self):
         request = self.rf.get('/')
-        request.user = AnonymousUser()
         context = self.step_page.get_context(request)
         self.assertTrue('checklists' in context)
         checklists = context['checklists']
         self.assertEqual(len(checklists), 1)
         self.assertTrue(isinstance(checklists[0], forms.ChecklistForm))
-        self.assertIsNone(checklists[0].project)
-
-    def test_get_context_with_project(self):
-        request = self.rf.get('/')
-        request.user = self.project.owner
-        context = self.step_page.get_context(request)
-        self.assertTrue('checklists' in context)
-        checklists = context['checklists']
-        self.assertEqual(len(checklists), 1)
-        self.assertTrue(isinstance(checklists[0], forms.ChecklistForm))
-        self.assertEqual(checklists[0].project, self.project)
-
-
-class ProjectTestCase(TestCase):
-
-    def test_create_user_creates_project(self):
-        user = factories.UserFactory()
-        projects = user.projects.all()
-        self.assertEquals(projects.count(), 1)
